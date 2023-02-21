d87328e9d96f2da4bba91cb7a938416d7449accd
sp <2451142030@qq.com>
2023-02-21 12:10:42 +0800

    1

--- 1__lesson/1__lesson/test.cpp
+++ 1__lesson/1__lesson/test.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 //C++上50节课，150小时
 //我们要讲三个部分的内容
 //1. C++语法  2.高阶数据结构   3.STL
@@ -121,12 +122,78 @@ int main()
 	return 0;
 }*/
 //命名空间也能够嵌套
+/*#include<stdio.h>
+int rand = 0;
+namespace sun
+{
+	int a = 0;
+	namespace sprise
+	{
+		int rand = 0;
+		void func()
+		{
+			printf("func()\n");
+		}
+		struct TreeNode
+		{
+			struct TreeNode* left;
+			struct TreeNode* right;
+			int val;
+		};
+	}
+}
+int main()
+{
+	printf("%p", rand);
+	printf("%p", sun::sprise::rand);
+	sun::sprise::func();
+	struct sun::sprise::TreeNode node;
+	return 0;
+}
+*/
+// 3. 同一个工程中允许存在多个相同名称的命名空间, 编译器最后会合成同一个命名空间中。
+// ps：一个工程中的test.h和上面test.cpp中两个同名命名空间会被合并成一个
 
+//但是我们如果是在测试，我们这样每一个变量 函数 类型都这么一个个在命名空间中引用
+//会造成极大的不便利
+//有没有方式 不指定就能去命名空间中去找
+//那么就是使用“using namespace 名称”
+// 
+//std是C++标准库的命名空间
+using namespace std;
+#include<iostream>//stream=流，输入输出流
+//使用cout要引用iostream文件
+int main()
+{
+	//cout 是ostream里面的对象
+	//endl代表的是换行符号，等价于\n
+	//流插入:
+	std::cout << "hello world!" << std::endl;
+	//  <<是流插入运算符
+	
+	//当然我们也能还用printf scanf
+	
+	//C++的特点是 自动识别类型
+	int i = 11;
+	double d = 11.11;
+	printf("%d,%f\n", i, d);
+	//
+	cout << i << d << endl;
 
+	//与其对应的
+	// cin
+	scanf("%d %lf", &i, &d);
+	printf("%d,%lf\n", i, d);
 
+	std::cin >> i >> d;
+	std::cout << i << "," << d << std::endl;
 
+	//  >> 流提取
+	return 0;
+}
 
-
-
-
+//我们如果嫌弃 直接全部展开std会多的话
+//可以自己指定几个展开，从而避免不必要的麻烦
+using std::cout;
+using std::cin;
 
